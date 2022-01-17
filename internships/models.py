2df1795517860aa277cad2f5821c496ee3665053
Douglas Sabando <douglas_sabando@hotmail.es>
2022-01-16 22:06:56 -0500

    Rename

--- internships/models.py
+++ internships/models.py
@@ -6,15 +6,15 @@ from utils.models import *
 class Internship(BaseModel):
     name = models.CharField(max_length=20)
     description = models.CharField(max_length=60, blank=True)
-    typeOfWorkDay = models.CharField(max_length=15)
+    type_of_workDay = models.CharField(max_length=15)
     requirements = ArrayField(models.CharField(max_length=15), size=5, blank=True, null=True)
     challenges = ArrayField(models.CharField(max_length=15), size=5, blank=True, null=True)
     profile = models.CharField(max_length=20)
-    startDate = models.DateField(auto_now=False, blank=True, null=True)
-    endDate = models.DateField(auto_now=False, blank=True, null=True)
-    durationMonths = models.IntegerField(max_length=2)
+    start_date = models.DateField(auto_now=False, blank=True, null=True)
+    end_date = models.DateField(auto_now=False, blank=True, null=True)
+    duration_months = models.IntegerField(max_length=2)
     remuneration = models.DecimalField(max_digits= 6, decimal_places=3)
-    haveRemuneration = models.BooleanField(default=False)
+    have_remuneration = models.BooleanField(default=False)
     owner_enterprise = models.ForeignKey("organization.Enterprise", on_delete=models.DO_NOTHING, related_name='enterprise_of_%(class)s')
 
 
@@ -26,7 +26,7 @@ class Internship(BaseModel):
 
 
 class Postulation(BaseModel):
-    postDate = models.DateField(auto_now=True, null=True)
+    post_date = models.DateField(auto_now=True, null=True)
     state = models.BooleanField(default=True)
     postulant = models.ForeignKey("user.Intern", on_delete=models.DO_NOTHING, related_name='profile_of_%(class)s')
     internship = models.ForeignKey(Internship, on_delete=models.DO_NOTHING, related_name='internship_of_%(class)s')
